6eab4fe11103c697c6d4d98d0edf2920805987ea
Ozodbek Umarov <ozodchik.krasavchik@gmail.com>
2024-06-20 08:56:25 +0500

    added OrderDtos

---
+++ Application/Common/DTOs/OrderDtos/AddOrderDto.cs
@@ -0,0 +1,13 @@
+﻿using Application.Common.DTOs.OrderDetailDtos;
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.Common.DTOs.OrderDtos;
+
+public class AddOrderDto
+{
+    [Required(ErrorMessage = "User ID is required.")]
+    public int UserId { get; set; }
+
+    [Required(ErrorMessage = "At least one order detail is required.")]
+    public List<AddOrderDetailDto> OrderDetails { get; set; } = new List<AddOrderDetailDto>();
+}
