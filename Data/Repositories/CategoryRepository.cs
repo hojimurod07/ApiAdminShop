b92eb7c6a258639602cb8fc769c612f59e0e5546
Ozodbek Umarov <ozodchik.krasavchik@gmail.com>
2024-06-20 07:03:37 +0500

    added CategoryRepository and GenericRepository

---
+++ Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using Data.DbContexts;
+using Data.Interfaces;
+using Domain.Entities;
+
+namespace Data.Repositories;
+
+public class CategoryRepository(AppDbContext dbContext) : GenericRepository<Category>(dbContext), ICategoryRepository
+{
+}
