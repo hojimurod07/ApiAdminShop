821449e6044bba6bc60aeb71fd4a1f84708adece
Ozodbek Umarov <ozodchik.krasavchik@gmail.com>
2024-06-20 06:27:49 +0500

    fixed domain

--- Data/Entities/BaseEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Data.Entities
-{
-    public abstract class BaseEntity
-    {
-        public int Id { get; set; }
-        public DateTime CreatedAd {  get; set; } = DateTime.UtcNow;
-        public DateTime UpdatedAd { get; set;} = DateTime.UtcNow;
-    }
-}
---
+++ Domain/Enums/Satus.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Enums;
+
+public enum Status
+{
+    todo = 1,
+    inprocess = 2,
+    done = 3,
+}
