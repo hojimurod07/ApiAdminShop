82e90e792eb794f7046f1c4cd0dc267e2db33798
Ozodbek Umarov <ozodchik.krasavchik@gmail.com>
2024-06-20 07:09:39 +0500

    added OrderDetailRepository

---
+++ Data/Repositories/OrderDetailRepository.cs
@@ -0,0 +1,9 @@
+﻿using Data.DbContexts;
+using Data.Interfaces;
+using Domain.Entities;
+
+namespace Data.Repositories;
+
+public class OrderDetailRepository(AppDbContext dbContext) : GenericRepository<OrderDetail>(dbContext), IOrderDetalRepository
+{
+}
