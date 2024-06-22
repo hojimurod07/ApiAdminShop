11a117b293e06f409f1e9b86c1cf39a4e8a38821
hojimurod07 <hojimurad2007@gmail.com>
2024-06-22 10:31:24 +0500

    controller qoshildii

---
+++ ApiAdminShop/Controllers/AdminsController.cs
@@ -0,0 +1,35 @@
+﻿using Application.Common.DTOs.UserDtos;
+using Application.Interfaces;
+using Application.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiAdminShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminsController(IAdminService adminService, IUserService userService) : ControllerBase
+    {
+        private readonly IAdminService _adminService = adminService;
+        private readonly IUserService _userService = userService;
+        [HttpPost("id")]
+        [Authorize(Roles = "SuperAdmin")]
+        public async Task<IActionResult> ChangeUserRoleAsync(int id)
+        {
+            await _adminService.ChangeUserRoleAsync(id);
+            return Ok();
+        }
+
+        [HttpPut]
+        [Authorize(Roles = "SuperAdmin")]
+        public async Task<IActionResult> UpdateUserAsync(int id, [FromForm] UpdateUserDto dto)
+        {
+            await _userService.UpdateAsync(id, dto);
+            return Ok();
+        }
+
+       
+ 
+    }
+}
