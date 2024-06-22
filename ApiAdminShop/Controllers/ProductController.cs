84e0eeb5d390ce84803abc0f0a65f9fb14e002d9
hojimurod07 <hojimurad2007@gmail.com>
2024-06-22 14:29:13 +0500

    products controller add

--- ApiAdminShop/Controllers/ProductController.cs
+++ ApiAdminShop/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
-﻿using Application.Interfaces;
+﻿using Application.Common.DTOs.ProductDtos;
+using Application.Common.Utils;
+using Application.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -10,6 +12,41 @@ namespace ApiAdminShop.Controllers
     public class ProductController(IProductService service) : ControllerBase
     {
         private readonly IProductService _service = service;
-       
+        [HttpPost]
+        [Authorize(Roles = "Admin, SuperAdmin")]
+        public async Task<IActionResult> CreateAsync([FromForm]AddProductDto dto)
+        {
+            await _service.CreateAsync(dto);
+            return Ok();
+
+        }
+        [HttpGet("id")]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            return Ok(await _service.GetByIdAsync(id));
+        }
+        [HttpGet("products")]
+        [Authorize]
+        public async Task<IActionResult> GetAllAsync([FromQuery] PaginationParams @params)
+        
+             => Ok(await _service.GetAllAsync(@params));
+
+        [HttpDelete("id")]
+        [Authorize(Roles = "SuperAdmin, Admin")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            await _service.DeleteAsync(id);
+            return Ok();
+        }
+
+        [HttpPut]
+        [Authorize(Roles = "SuperAdmin, Admin")]
+        public async Task<IActionResult> UpdateAsync([FromForm] ProductDto dto)
+        {
+            await _service.UpdateAsync(dto);
+            return Ok();
+        }
+
+
     }
 }
