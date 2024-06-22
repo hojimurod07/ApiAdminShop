67b1a70c6f124b90b7857e35485c945e00d71da6
Ozodbek Umarov <ozodchik.krasavchik@gmail.com>
2024-06-22 06:28:12 +0500

    fixed PasswordHasher and added RedisService

---
+++ Application/Interfaces/IRedisService.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Interfaces;
+
+public interface IRedisService
+{
+    public Task DeleteAsync(string key);
+    public Task<string?> GetAsync(string key);
+    public Task SetAsync(string key, string value);
+}
---
+++ Application/Services/RedisService.cs
@@ -0,0 +1,24 @@
+﻿using Application.Interfaces;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Application.Services;
+
+public class RedisSerrvice(IDistributedCache cache) : IRedisService
+{
+    private readonly IDistributedCache _cache = cache;
+
+    public async Task DeleteAsync(string key)
+        => await _cache.RemoveAsync(key);
+
+    public async Task<string?> GetAsync(string key)
+        => await _cache.GetStringAsync(key);
+
+    public async Task SetAsync(string key, string value)
+    {
+        await _cache.SetStringAsync(key, value, new DistributedCacheEntryOptions()
+        {
+            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(3),
+            SlidingExpiration = TimeSpan.FromMinutes(1),
+        });
+    }
+}
