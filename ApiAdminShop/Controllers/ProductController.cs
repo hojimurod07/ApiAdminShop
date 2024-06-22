eec2e7f6c3e3959229fa62a522573c6624b5c0e3
hojimurod07 <hojimurad2007@gmail.com>
2024-06-22 14:14:41 +0500

    hammasi yaxshi

--- ApiAdminShop/Controllers/ProductController.cs
+++ ApiAdminShop/Controllers/ProductController.cs
@@ -1,11 +1,15 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiAdminShop.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProductController : ControllerBase
+    public class ProductController(IProductService service) : ControllerBase
     {
+        private readonly IProductService _service = service;
+       
     }
 }
--- ApiAdminShop/Controllers/UsersController.cs
+++ ApiAdminShop/Controllers/UsersController.cs
@@ -1,11 +1,47 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Application.Common.DTOs.UserDtos;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiAdminShop.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UsersController : ControllerBase
+    public class UsersController(IUserService userService): ControllerBase
     {
+        private readonly IUserService _userService = userService;
+
+        [HttpGet("{id}")]
+        [Authorize(Roles = "SuperAdmin, Admin")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            return Ok(await _userService.GetByIdAsync(id));
+        }
+
+        [HttpGet("users")]
+        [Authorize(Roles = "Admin, SuperAdmin")]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            return Ok(await _userService.GetAllAsync());
+        }
+
+        [HttpPut]
+        [Authorize]
+        public async Task<IActionResult> UpdateAsync([FromForm] UpdateUserDto dto)
+        {
+            var id = int.Parse(HttpContext.User.FindFirst("Id")!.Value);
+
+            await _userService.UpdateAsync(id, dto);
+            return Ok();
+        }
+
+        [HttpDelete("id")]
+        [Authorize]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            await _userService.DeleteAsync(id);
+            return Ok();
+        }
     }
 }
